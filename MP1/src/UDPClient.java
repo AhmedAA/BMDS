0ea7d1554b412e6903af8be6e01a6d2c879bb503
Ananochi <rloc@itu.dk>
2014-09-11 15:38:27 +0200

    moar UDP party!

---
+++ MP1/src/UDPClient.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Rasmus on 11-09-2014.
+ */
+
+import java.net.*;
+import java.io.*;
+
+public class UDPClient {
+
+    public static void main(String args[]){
+        // args give message contents and destination hostname
+        DatagramSocket aSocket = null;
+        try {
+            aSocket = new DatagramSocket();
+            byte [] m = args[0].getBytes();
+            InetAddress aHost = InetAddress.getByName(args[1]);
+            int serverPort = 8080;
+            DatagramPacket request =
+                    new DatagramPacket(m,  args[0].length(), aHost, serverPort);
+            aSocket.send(request);
+
+            // code below uncommented, since we do not want response here!
+
+            //byte[] buffer = new byte[1000];
+            //DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+            //aSocket.receive(reply);
+            //System.out.println("Reply: " + new String(reply.getData()));
+        }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
+        }catch (IOException e){System.out.println("IO: " + e.getMessage());
+        }finally {if(aSocket != null) aSocket.close();}
+    }
+}
--- MP1/src/UDPForwarder.java
+++ MP1/src/UDPForwarder.java
@@ -1,10 +1,15 @@
+/**
+ * Created by Rasmus on 11-09-2014.
+ */
 import java.net.*;
 import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-class UDPForwarder{
+public class UDPForwarder {
+
+    private InetAddress host = null;
 
     public static void UDPForwarder(String host, int p1, int p2) {
 
@@ -15,7 +20,7 @@ class UDPForwarder{
             // listen at p1
             p1Socket = new DatagramSocket(p1);
             System.out.println("Listening at " + p1Socket.getLocalAddress() + ":" +
-                p1Socket.getLocalPort());
+                    p1Socket.getLocalPort());
 
             byte[] buffer = new byte[1000];
             while(true){
@@ -26,19 +31,29 @@ class UDPForwarder{
                 // recieving data on p1
                 p1Socket.receive(request);
                 System.out.println("Received " +
-                    request.getLength() + " bytes" +
-                    " from " +
-                    request.getAddress().toString() +
-                    ":" + request.getPort());
+                        request.getLength() + " bytes" +
+                        " from " +
+                        request.getAddress().toString() +
+                        ":" + request.getPort());
+
+                // connecting to p2Socket
+                p2Socket.connect(InetAddress.getByName(host), p2);
+                System.out.println("Now looking at " + host + " at port " + p2);
+
+                // BREAK BREAK BREAK !!!
 
                 // creating and forwarding to p2
-                p2Socket = new DatagramSocket(p2);
-                DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(),
-                            request.getAddress(), request.getPort());
+                //p2Socket = new DatagramSocket(p2);
+                //DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(),
+                  //      request.getAddress(), request.getPort());
 
-                p2Socket.send(reply);
+                //p2Socket.send(reply);
 
             }
+
+            // connect to 'h' at 'p2' (the destination host and port)
+
+
         }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
         }catch (IOException e) {System.out.println("IO: " + e.getMessage());
         }finally {if(p2Socket != null) p2Socket.close();}
