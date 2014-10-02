fdc99ffbea3bd57af8fe8a578c59e04b11187c90
Ananochi <rloc@itu.dk>
2014-10-02 15:07:00 +0200

    kaoscommit

--- MP2/src/Controller.java
+++ MP2/src/Controller.java
@@ -1,11 +1,123 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+
 /**
  * Created by ahmed on 29/09/14.
  * Should simply print out responses from started Sink/Source threads
  */
 public class Controller {
 
-    public static void main(String args[])
+    public static void main(String args[]) throws Exception
     {
-        //set some variables, start a method to listen.
+        ServerSocket listenSourceSocket = null;
+
+        try {
+
+            int sourcePort = 7896;
+
+            listenSourceSocket = new ServerSocket(sourcePort);
+
+            // source
+            while (true) {
+
+                Socket sourceSocket = listenSourceSocket.accept();
+                System.out.println("Controller: Accepted SOURCE socket: " + sourceSocket.getLocalAddress());
+
+                new SourceConnection(sourceSocket);
+            }
+
+
+        } finally {
+            if (listenSourceSocket != null)
+                listenSourceSocket.close();
+        }
+
     }
+}
+
+class SourceConnection extends Thread {
+    DataInputStream in;
+    DataOutputStream out;
+    Socket clientSocket;
+
+    public SourceConnection(Socket aClientSocket) throws Exception {
+
+        clientSocket = aClientSocket;
+        in = new DataInputStream(clientSocket.getInputStream());
+        out = new DataOutputStream(clientSocket.getOutputStream());
+
+        this.start();
+
+    }
+
+    public void run() {
+
+        try {
+
+            while(true) {
+
+                String data = in.readUTF();
+                System.out.println("Data: " + data);
+
+                }
+            catch (Exception e) {
+                System.out.println("Connection died:" + e.getMessage());
+            }
+
+        final Thread sourceThread = new Thread(new Runnable() {
+
+
+
+            }
+        });
+
+        outputThread.start();
+
+    }
+
+}
+
+class Connection extends Observable {
+    DataInputStream in;
+    DataOutputStream out;
+    Socket clientSocket;
+
+    public Connection(Socket aClientSocket) throws Exception {
+
+        clientSocket = aClientSocket;
+        in = new DataInputStream(clientSocket.getInputStream());
+        out = new DataOutputStream(clientSocket.getOutputStream());
+        final DataSink sink = new DataSink();
+
+        final Thread outputThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                addObserver(sink);
+                String input = null;
+
+                while (true) {
+                    try {
+                        input = in.readUTF();
+                        if (input != null) {
+                            setChanged();
+                            notifyObservers(input);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        outputThread.start();
+
+    }
+
 }
--- MP2/src/DataSink.java
+++ MP2/src/DataSink.java
@@ -1,7 +1,13 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.omg.CORBA.portable.ResponseHandler;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Scanner;
 
 /**
@@ -9,8 +15,57 @@ import java.util.Scanner;
  * Should be a threaded socket server, that handles client (source) connections, handle the data sent, and sends this
  * to the controller.
  */
-public class DataSink {
+public class DataSink implements Observer {
 
+    public static void main(String args[]) throws Exception
+    {
 
+        Thread inputThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                Socket s = null;
+                int serverPort = 9090;
+
+                try {
+                    s = new Socket("localhost", serverPort);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                DataOutputStream out = null;
+                try {
+                    out = new DataOutputStream(s.getOutputStream());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Scanner scan = new Scanner(System.in);
+                String input = "";
+                while (true) {
+                    System.out.println("Type something: ");
+                    input = scan.nextLine();
+                    try {
+                        out.writeUTF(input);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Input: "+input);
+                }
+            }
+        });
+
+        inputThread.start();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+        String resp;
+        if (arg instanceof String) {
+            resp = (String) arg;
+            System.out.println("\nReceived Response: " + resp);
+        }
+    }
 }
 
