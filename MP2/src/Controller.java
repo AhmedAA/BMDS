cb643bf91010f05ad0fae9914dc09b427cfa91fc
Ahmed Al Aqtash <ahmed.aqtash@gmail.com>
2014-10-16 11:53:54 +0200

    ALMOST THERE

--- MP2/src/Controller.java
+++ MP2/src/Controller.java
@@ -12,8 +12,14 @@ import java.util.*;
  */
 public class Controller {
 
-    public static void main(String args[]) throws Exception
-    {
+    List<Socket> sinks = new ArrayList<Socket>();
+
+    public static void main(String[] args) throws Exception {
+        new Controller().start();
+    }
+
+
+    public void start() throws Exception {
         ServerSocket listenSocket = null;
         int sourceNumber = 0;
         int sinkNumber = 0;
@@ -38,12 +44,12 @@ public class Controller {
                     new SourceConnection(sourceSocket, sourceNumber);
                 }
 
-                if (initmsgString.equals("sink")){
+                if (initmsgString.equals("sink")) {
                     System.out.println("Controller: Sink accepted");
                     sinkNumber += 1;
                     //Socket sinkSocket = new Socket("localhost", sinkPort);
                     sinkPort += 1;
-                    new Handler(sourceSocket, sinkNumber);
+                    setSinks(sourceSocket, sinkNumber);
                 }
             }
         } finally {
@@ -52,83 +58,89 @@ public class Controller {
         }
 
     }
-}
 
-class SourceConnection extends Thread {
-    DataInputStream in;
-    DataOutputStream out;
-    Socket clientSocket;
-    int sourceNumber;
-
-    public SourceConnection(Socket aClientSocket, int sn) throws Exception
-    {
-        clientSocket = aClientSocket;
-        sourceNumber = sn;
-        in = new DataInputStream(clientSocket.getInputStream());
-        out = new DataOutputStream(clientSocket.getOutputStream());
-        this.start();
+    public void setSinks(Socket sinkSocket, int sinkNumber) {
+        sinks.add(sinkSocket);
     }
 
-    public void run()
-    {
-        try {
-            while(true) {
-                List<Socket> sinks = Handler.getSinks();
-                String data = in.readUTF();
-                System.out.println(data);
-                for(Socket sinkSocket : sinks){
-                    out.writeUTF("A" + sourceNumber + " "  + data);
+    private class SourceConnection extends Thread {
+        DataInputStream in;
+        DataOutputStream out;
+        Socket clientSocket;
+        int sourceNumber;
+
+        public SourceConnection(Socket aClientSocket, int sn) throws Exception {
+            clientSocket = aClientSocket;
+            sourceNumber = sn;
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+            this.start();
+        }
+
+        public void run() {
+            try {
+                while (true) {
+                    String data = in.readUTF();
+                    System.out.println(data);
+
                 }
+            } catch (Exception e) {
+                System.out.println("Connection died:" + e.getMessage());
             }
-        } catch (Exception e) {
-            System.out.println("Connection died:" + e.getMessage());
+        }
+    }
+
+    public void publish(String message) throws IOException {
+        for(Socket sinkSocket : sinks)
+        {
+            DataOutputStream sinkOut = new DataOutputStream(sinkSocket.getOutputStream());
+            sinkOut.writeUTF(message);
         }
     }
 }
 
-class SinkConnection extends Thread {
-    Socket clientSocket;
-    DataOutputStream out;
-    DataInputStream in;
-    int sinkNumber;
-
-    public SinkConnection(Socket aClientSocket, int sn) throws Exception {
-        clientSocket = aClientSocket;
-        out = new DataOutputStream(clientSocket.getOutputStream());
-        in = new DataInputStream(clientSocket.getInputStream());
-        sinkNumber = sn;
-        this.start();
-    }
-
-    public void run()
-    {
-
-    }
-}
-
-class Handler {
-    private static List<Socket> sinks;
-    //List<Socket> sinks = new ArrayList<Socket>();
-    Socket sinkSocket;
-    int sinkNumber;
-
-    public Handler(Socket socket, int sinkNumber) throws IOException {
-        sinkSocket = socket;
-        this.sinkNumber = sinkNumber;
-        DataOutputStream sinkOut = new DataOutputStream(sinkSocket.getOutputStream());
-        //sinkOut.writeUTF("HEEEEEEEEEEEJ");
-        //System.out.println("Burde have skrevet");
-
-        addSinks(sinkSocket, sinkNumber);
-    }
-
-    public static List<Socket> getSinks()
-    {
-        return sinks;
-    }
-
-    public void addSinks(Socket sinkSocket, int sinkNumber) throws IOException {
-        System.out.println("Sink added");
-        sinks.add(sinkSocket);
-    }
-}
+//class SinkConnection extends Thread {
+//    Socket clientSocket;
+//    DataOutputStream out;
+//    DataInputStream in;
+//    int sinkNumber;
+//
+//    public SinkConnection(Socket aClientSocket, int sn) throws Exception {
+//        clientSocket = aClientSocket;
+//        out = new DataOutputStream(clientSocket.getOutputStream());
+//        in = new DataInputStream(clientSocket.getInputStream());
+//        sinkNumber = sn;
+//        this.start();
+//    }
+//
+//    public void run()
+//    {
+//
+//    }
+//}
+//
+//class Handler {
+//    //List<Socket> sinks = new ArrayList<Socket>();
+//    Socket sinkSocket;
+//    int sinkNumber;
+//
+//    public Handler(Socket socket, int sinkNumber) throws IOException {
+//        sinkSocket = socket;
+//        this.sinkNumber = sinkNumber;
+//        DataOutputStream sinkOut = new DataOutputStream(sinkSocket.getOutputStream());
+//        //sinkOut.writeUTF("HEEEEEEEEEEEJ");
+//        //System.out.println("Burde have skrevet");
+//
+//        addSinks(sinkSocket, sinkNumber);
+//    }
+//
+//    public List<Socket> getSinks()
+//    {
+//        return sinks;
+//    }
+//
+//    public void addSinks(Socket sinkSocket, int sinkNumber) throws IOException {
+//        System.out.println("Sink added");
+//        sinks.add(sinkSocket);
+//    }
+//}
