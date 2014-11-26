d88ce6e374337c468934d19ca5ab55ddaf9719a4
AhmedAA <ahmed.aqtash@gmail.com>
2014-11-26 15:30:55 +0100

    put done

--- MP3/src/Message.java
+++ MP3/src/Message.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
  * Created by ahmed on 13/11/14.
  */
 public class Message implements Serializable {
-    public void Message(){
-
+    int key;
+    String message;
     }
-}
--- MP3/src/Put.java
+++ MP3/src/Put.java
@@ -1,10 +1,31 @@
-import java.io.Serializable;
+import java.io.*;
+import java.net.Socket;
 
 /**
  * Created by ahmed on 13/11/14.
  */
 public class Put implements Serializable{
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        Message m = new Message();
+        Socket nodeSocket = null;
 
+        String nodeIP = args[0];
+        int nodePort = Integer.parseInt(args[1]);
+
+        nodeSocket = new Socket(nodeIP, nodePort);
+
+        m.key = Integer.parseInt(args[2]);
+        m.message = args[3];
+
+        ObjectOutputStream nodeOut = new ObjectOutputStream(nodeSocket.getOutputStream());
+        nodeOut.writeObject(m);
+        nodeOut.close();
+
+        // Old code
+        //        FileOutputStream fs = new FileOutputStream("messagetmp");
+        //        ObjectOutputStream out = new ObjectOutputStream(fs);
+        //
+        //        out.writeObject(m);
+        //        out.close();
     }
 }
