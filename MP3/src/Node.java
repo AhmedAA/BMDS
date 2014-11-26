dfce8d9274808ec98efadf1ebbd342b1bb1b8658
Ananochi <rloc@itu.dk>
2014-11-26 16:05:04 +0100

    Chaos nodes

--- MP3/src/Node.java
+++ MP3/src/Node.java
@@ -1,3 +1,5 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -12,6 +14,7 @@ public class Node {
     private String next = null;
     private String nextnext = null;
     private int listenPort;
+    private Socket tempSocket;
 
     public Node(int port, String... friendVars) {
 
@@ -25,8 +28,24 @@ public class Node {
             listenPort = port;
             ServerSocket connectionSocket = new ServerSocket(listenPort, 0, localHost);
 
-            // debug SOUT
+            // debug SOUTs
             System.out.println("Node at " + localHost + ":" + listenPort + " is now running!");
+
+            // trigger event - "i got friends!"
+            if (friendVars.length > 0) {
+
+                // setting "prev" to the new friend
+                prev = friendVars[0];
+
+                // my friend needs to know about our friendship!
+                String[] friendData = friendVars[0].split(":");
+                Socket friendSocket = new Socket(friendData[0], Integer.parseInt(friendData[1]));
+
+                tempSocket = friendSocket;
+                DataOutputStream out = new DataOutputStream(tempSocket.getOutputStream());
+                out.writeUTF("HEJ MED DIG NODE?"); // test streng!
+
+            }
 
         } catch (IOException e) {
 
@@ -39,7 +58,28 @@ public class Node {
 
             while (true) {
 
-                System.out.println("Jeg en tråd: " + listenPort);
+                System.out.println("------------------------------");
+                System.out.println("Jeg er tråd: " + listenPort);
+                System.out.println("Previous Node: " + prev);
+                System.out.println("Next Node: " + next);
+                System.out.println("Next next Node: " + nextnext);
+                System.out.println("------------------------------");
+
+                if (tempSocket != null) {
+                    try {
+                        DataInputStream in = new DataInputStream(tempSocket.getInputStream());
+
+                        while (true) {
+
+                            next = in.readUTF();
+
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
 
                 try {
 
