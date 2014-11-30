8e3e37829a96d6438dbd9c5f38c80cb52a4ecb54
AhmedAA <ahmed.aqtash@gmail.com>
2014-11-30 17:54:22 +0100

    stage

--- MP3/src/Get.java
+++ MP3/src/Get.java
@@ -1,8 +1,17 @@
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * Created by ahmed on 13/11/14.
  */
 public class Get {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        Message m = new Message();
+        Socket nodeSocket = null;
 
+        String nodeIP = args[0];
+        int nodePort = Integer.parseInt(args[1]);
+
+        nodeSocket = new Socket(nodeIP, nodePort);
     }
 }
